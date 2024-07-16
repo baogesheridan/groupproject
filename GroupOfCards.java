4b89299a6ceecf533fb046c62f9d0854133cbd46
baogesheridan <zhaoba@sheridancollege.ca>
2024-07-15 23:08:47 -0400

    Add files via upload
    final code v1.0

---
+++ GroupOfCards.java
@@ -0,0 +1,31 @@
+package src.ca.sheridancollege.project;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class GroupOfCards {
+
+    private ArrayList<Card> cards;
+    private int size;
+
+    public GroupOfCards(int size) {
+        this.size = size;
+        this.cards = new ArrayList<>(size);
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
